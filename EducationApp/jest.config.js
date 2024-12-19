5db95364533d8c4a23cc327e67ed7d61b3603f5c
rbcaglobalsg <rbca.global@gmail.com>
2024-12-20 02:51:44 +0800

    Set testEnvironment to jsdom in jest config to fix requestAnimationFrame issue

--- EducationApp/jest.config.js
+++ EducationApp/jest.config.js
@@ -1,12 +1,12 @@
-// EducationApp/jest.config.js
 module.exports = {
   preset: 'react-native',
   setupFiles: ['./jestSetup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|react-native|@react-native|react-navigation|react-native-gesture-handler|@stripe|@react-native-async-storage|react-native-reanimated))'
+    'node_modules/(?!((jest-)?react-native|react-native|@react-native|react-navigation|react-native-gesture-handler|@stripe))'
   ],
   moduleNameMapper: {
     '\\.(png|jpe?g|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
     '^@stripe/stripe-react-native$': '<rootDir>/__mocks__/@stripe/stripe-react-native.js'
-  }
+  },
+  testEnvironment: 'jsdom', // 이 줄을 추가
 };
