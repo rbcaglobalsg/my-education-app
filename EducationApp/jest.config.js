7e0dbe898b15d921717fabc891cd5d740d94dde6
rbcaglobalsg <rbca.global@gmail.com>
2024-12-20 01:09:52 +0800

    Mock @stripe/stripe-react-native and StripeSdk native module

--- EducationApp/jest.config.js
+++ EducationApp/jest.config.js
@@ -1,9 +1,8 @@
-// EducationApp/jest.config.js
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['./jestSetup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|react-native|@react-native|react-navigation|react-native-gesture-handler))'
+    'node_modules/(?!((jest-)?react-native|react-native|@react-native|react-navigation|react-native-gesture-handler|@stripe))'
   ],
   moduleNameMapper: {
     '\\.(png|jpe?g|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
