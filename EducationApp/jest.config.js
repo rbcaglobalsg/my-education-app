0e0e19854fc09853a1b7cf1acb061352e3999b20
rbcaglobalsg <rbca.global@gmail.com>
2024-12-20 00:51:40 +0800

    Add moduleNameMapper for image files in Jest config

--- EducationApp/jest.config.js
+++ EducationApp/jest.config.js
@@ -1,8 +1,11 @@
 // EducationApp/jest.config.js
 module.exports = {
   preset: 'react-native',
-  setupFilesAfterEnv: ['./jestSetup.js'], // jestSetup.js 파일 로드
+  setupFilesAfterEnv: ['./jestSetup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!react-native|react-native-gesture-handler|@react-native|react-navigation)',
+    'node_modules/(?!react-native|react-native-gesture-handler|@react-native|@react-navigation)',
   ],
+  moduleNameMapper: {
+    '\\.(png|jpe?g|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  }
 };
